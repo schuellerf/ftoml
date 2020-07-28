e29ddd1b424efb4fd66013bc14f3694c23881ece
Florian Schüller <florian.schueller@gmail.com>
2020-07-28 23:13:37 +0200

    First tests

---
+++ tests/test_ftoml.py
@@ -0,0 +1,32 @@
+import toml
+import ftoml
+import pytest
+import sys
+import os
+
+# helper to load files named the same as the test-function
+def _load_compare(func_name):
+    d=os.path.dirname(os.path.realpath(__file__))
+    assert ftoml.load(os.path.join(d,f"{func_name}.in.ftoml")) == toml.load(os.path.join(d,f"{func_name}.out.toml"))
+
+# TBD: dynamically generate those from file listing
+def test_simple_fstring():
+    _load_compare(sys._getframe().f_code.co_name)
+
+def test_recursive_variables():
+    _load_compare(sys._getframe().f_code.co_name)
+
+def test_inheritance():
+    _load_compare(sys._getframe().f_code.co_name)
+
+def test_defaults():
+    _load_compare(sys._getframe().f_code.co_name)
+
+def test_absolute_reference():
+    _load_compare(sys._getframe().f_code.co_name)
+
+def test_arrays():
+    _load_compare(sys._getframe().f_code.co_name)
+
+def test_leave_others():
+    _load_compare(sys._getframe().f_code.co_name)
